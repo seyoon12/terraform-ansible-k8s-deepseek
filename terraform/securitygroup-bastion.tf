a7f777e9fa6f213e7f6f849589f4ca87f2c5435e
seyoon12 <138763053+seyoon12@users.noreply.github.com>
2025-03-21 00:08:59 +0900

    Update securitygroup-bastion.tf

--- terraform/securitygroup-bastion.tf
+++ terraform/securitygroup-bastion.tf
@@ -1,8 +1,17 @@
 module "bastion_sg" {
     source  = "terraform-aws-modules/security-group/aws"
-    name = "bastion_sg"
-    vpc_id = aws_vpc.main.id
-    ingress_rules = ["all-all"]
-    ingress_cidr_blocks = ["0.0.0.0/0"]
+    name    = "bastion_sg"
+    vpc_id  = aws_vpc.main.id
+
+    ingress_with_cidr_blocks = [
+        { rule = "ssh-tcp", cidr_blocks = ["0.0.0.0/0"] },      # 22
+        { rule = "http-80-tcp", cidr_blocks = ["0.0.0.0/0"] },  # 80
+        { rule = "https-443-tcp", cidr_blocks = ["0.0.0.0/0"] } # 443
+    ]
+
+    ingress_with_cidr_blocks = [
+        { from_port = 11434, to_port = 11434, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+    ]
+
     egress_rules = ["all-all"]
 }
