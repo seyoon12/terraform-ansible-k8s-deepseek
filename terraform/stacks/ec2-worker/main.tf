058122ef5dddecca2513cb48f0be1de36a7b3fe3
seyoon12 <138763053+seyoon12@users.noreply.github.com>
2025-03-29 22:08:01 +0900

    Update main.tf

--- terraform/stacks/ec2-worker/main.tf
+++ terraform/stacks/ec2-worker/main.tf
@@ -15,10 +15,6 @@ module "vpc" {
   source = "../../modules/vpc"
 }
 
-module "keypair" {
-  source = "../../modules/keyPair"
-}
-
 module "sg" {
   source = "../../modules/sg"
   vpc_id = module.vpc.vpc_id
@@ -28,7 +24,7 @@ module "worker_node_01" {
   source            = "../../modules/ec2-worker"
   subnet_id         = module.vpc.public_subnet_id
   availability_zone = module.vpc.availability_zone_0
-  key_name          = module.keypair.k8s_keyname
+  key_name          = "k8s" # ec2-master에서 사용 중인 key
   workernodes_sg_group_id = [module.sg.workernodes_sg_group_ids]
   user_data         = file("${path.module}/../../scripts/worker-userdata.sh")
   ami               = var.ami
@@ -39,7 +35,7 @@ module "worker_node_02" {
   source            = "../../modules/ec2-worker"
   subnet_id         = module.vpc.public_subnet_id
   availability_zone = module.vpc.availability_zone_0
-  key_name          = module.keypair.k8s_keyname
+  key_name          = "k8s" # ec2-master에서 사용 중인 key
   workernodes_sg_group_id = [module.sg.workernodes_sg_group_ids]
   user_data         = file("${path.module}/../../scripts/worker-userdata.sh")
   ami               = var.ami
