7ad415602743dac4a2da21cac6e4be6c5647cb35
seyoon12 <138763053+seyoon12@users.noreply.github.com>
2025-03-29 21:53:21 +0900

    Update main.tf

--- terraform/stacks/ec2-worker/main.tf
+++ terraform/stacks/ec2-worker/main.tf
@@ -24,24 +24,13 @@ module "sg" {
   vpc_id = module.vpc.vpc_id
 }
 
-module "worker_node_01" {
-  source            = "../../modules/ec2-worker"
-  subnet_id         = module.vpc.public_subnet_id
+module "master_node" {
+  source            = "../../modules/ec2-master"
+  subnet_id         = module.vpc.public_subnet_id   
   availability_zone = module.vpc.availability_zone_0
   key_name          = module.keypair.k8s_keyname
-  workernodes_sg_group_ids = [module.sg.workernodes_sg_group_ids]
-  user_data             = file("${path.module}/scripts/worker-userdata.sh")
-  ami               = var.ami
-  instance_type     = var.instance_type
-}
-
-module "worker_node_02" {
-  source            = "../../modules/ec2-worker"
-  subnet_id         = module.vpc.public_subnet_id
-  availability_zone = module.vpc.availability_zone_0
-  key_name          = module.keypair.k8s_keyname
-  workernodes_sg_group_ids = [module.sg.workernodes_sg_group_ids]
-user_data             = file("${path.module}/scripts/worker-userdata.sh")
+  masternode_sg_group_id = [module.sg.masternode_sg_group_ids]
+  user_data         = file("${path.module}/../../scripts/master-userdata.sh")
   ami               = var.ami
   instance_type     = var.instance_type
 }
