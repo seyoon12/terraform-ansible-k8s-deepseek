4081de586953f3c52b00d1f132f4c67a0fab1479
seyoon12 <138763053+seyoon12@users.noreply.github.com>
2025-03-29 23:24:46 +0900

    Update outputs.tf

--- terraform/stacks/ec2-master/outputs.tf
+++ terraform/stacks/ec2-master/outputs.tf
@@ -2,3 +2,23 @@ output "k8s_private_key" {
   value     = module.keypair.k8s_private_key
   sensitive = true
 }
+
+output "private_ip" {
+  value = module.master_node.private_ip
+}
+
+output "public_ip" {
+  value = module.master_node.public_ip
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_id" {
+  value = module.vpc.public_subnet_id
+}
+
+output "availability_zone_0" {
+  value = module.vpc.availability_zone_0
+}
